595538d813ec4f0e41c0f50070446baf8ac17f68
Ilya Kashcheev <kasheev_ilya.01@mail.ru>
2023-03-19 16:45:11 +0300

    Feat: (SelectPage.vue) New examples and data from store
    New examples and data from store
    
    Data from vuex store (getBerries).
    
    New examples:
    Without closing on select
    With closing on select
    With custom labels
    Without options
    Disabled select

--- vue2/app/src/views/SelectPage.vue
+++ vue2/app/src/views/SelectPage.vue
@@ -1,15 +1,49 @@
 <template>
   <div class="about">
     <h1>Select</h1>
-    <div>
+    <div style="max-width: 400px;">
+      <h2>Select without closing on select</h2>
       <SelectComponent
-        v-model="selectedOption"
-        :options="meals"
+        v-model="selectedOption1"
+        :options="berries"
         :close-on-select="false"
-        :disabled="false"
+        no-data-message="Доступных ягод нет"
+        placeholder="Выберите ягоду"
+      >
+      </SelectComponent>
+      <h2>Select with closing on select</h2>
+      <SelectComponent
+        v-model="selectedOption2"
+        :options="berries"
+        :close-on-select="true"
+        no-data-message="Доступных ягод нет"
+        placeholder="Выберите ягоду"
+      >
+      </SelectComponent>
+      <h2>Select with custom labels</h2>
+      <SelectComponent
+        v-model="selectedOption3"
+        :options="berries"
         :custom-label="customLabel"
-        no-data-message="Доступных приёмов пищи нет"
-        placeholder="Выберите приём пищи"
+        no-data-message="Доступных ягод нет"
+        placeholder="Выберите ягоду"
+      >
+      </SelectComponent>
+      <h2>Select without options</h2>
+        <SelectComponent
+        v-model="selectedOption4"
+        no-data-message="Доступных ягод нет"
+        placeholder="Выберите ягоду"
+      >
+      </SelectComponent>
+      <h2>Disabled select</h2>
+      <SelectComponent
+        v-model="selectedOption5"
+        :options="berries"
+        :disabled="true"
+        :custom-label="customLabel"
+        no-data-message="Доступных ягод нет"
+        placeholder="Выберите ягоду"
       >
       </SelectComponent>
     </div>
@@ -26,45 +60,26 @@ export default {
   },
   data() {
     return {
-      selectedOption: "dinner",
+      selectedOption1: "strawberry",
+      selectedOption2: "strawberry",
+      selectedOption3: "strawberry",
+      selectedOption4: "strawberry",
+      selectedOption5: "strawberry",
+      selectedOption6: "strawberry",
     };
   },
 
   methods: {
     customLabel(option)
     {
-      return (`${option.name} - ${option.value}`);
+      return (`${option.name} - ${option.color}`);
     }
   },
 
   computed: {
-    meals() {
-      return [
-        {
-          name: "Завтрак",
-          value: "breakfast",
-        },
-        {
-          name: "Обед",
-          value: "lunch",
-        },
-        {
-          name: "Ужин",
-          value: "dinner",
-        },
-        {
-          name: "Завтрак",
-          value: "breakfast1",
-        },
-        {
-          name: "Обед",
-          value: "lunch1",
-        },
-        {
-          name: "Ужин",
-          value: "dinner1",
-        },
-      ];
+
+    berries(){
+        return this.$store.getters['berries/getBerries']
     },
   },
 };
