ee8d8f5a78057765a090862ae4d0fb24887d8d95
twtzptr <wildhare2020@yandex.ru>
2023-03-29 02:59:41 +0300

    modal window component

---
+++ vue2/components/modalWindow/Modal.vue
@@ -0,0 +1,60 @@
+<template>
+  <Transition name="modal-fade"
+              enter-active-class="modal-fade--smooth"
+              enter-class="modal-fade--invisible"
+              leave-active-class="modal-fade--smooth"
+              leave-to-class="modal-fade--invisible"
+  >
+    <div v-if="opened" ref="wrapper" class="modal__wrapper" :class="{'modal--active': opened}" @click="onClose">
+      <div class="modal__content">
+        <div class="modal-header" v-if="title || withCloseButton">
+          <div><span class="modal-header__title">{{ title }}</span></div>
+          <div v-if="withCloseButton" id="close-modal-button" class="modal-header__close-button"
+               @click="onCloseByButton">
+            <svg style="text-align: right" width="24px" height="24px" viewBox="0 0 24 24" fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z"
+                    fill="#000000"/>
+            </svg>
+          </div>
+        </div>
+        <slot></slot>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script>
+export default {
+  name: `Modal`,
+  props: {
+    opened: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      default: null,
+    },
+    withCloseButton: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  methods: {
+    onClose(e) {
+      if (e.target === this.$refs.wrapper) {
+        this.$emit('onClose');
+      }
+    },
+    onCloseByButton() {
+      this.$emit('onClose');
+    }
+  },
+}
+</script>
+
+<style lang="less">
+@import "styles/styles.less";
+</style>
