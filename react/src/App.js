edaa0957d34101814c2088fad27e08d81ea4f107
RepMonstar <azaza.lekha@mail.ru>
2023-03-29 17:05:33 +0300

    Solution without node_modules and package-lock

---
+++ react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StarRating from './StarRating';
+
+function App() {
+  return (
+    <div className="App">
+     <StarRating/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/src/StarRating.js
@@ -0,0 +1,34 @@
+import React, { Component, useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const StarRating = () => {
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}         
+            />
+            <FaStar
+              className="star"
+              color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+              size={100}
+              onMouseEnter = {()=> setHover(ratingValue)}
+              onMouseLeave = {()=> setHover(null)}
+            />
+          </label>
+        );
+      })}
+      <p>Рейтинг: {rating}</p>
+    </div>
+  );
+};
+
+export default StarRating;
