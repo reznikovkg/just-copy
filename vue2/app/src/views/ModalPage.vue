e4e9e0c2cb46727c0b8c4d87febdd2ec71938320
twtzptr <wildhare2020@yandex.ru>
2023-03-30 18:29:51 +0300

    chore improvements

--- vue2/app/src/views/ModalPage.vue
+++ vue2/app/src/views/ModalPage.vue
@@ -3,7 +3,7 @@
         <ButtonComponent v-for="(modal, index) in modalsData" :key="'button'+index" @click="() => addModal(modal)" size="large">
             {{ modal.name }}
         </ButtonComponent>
-        <div v-for="(modal, index) in modals" :key="'modal'+index" style="margin: 10px">
+        <div v-for="(modal, index) in getModals" :key="'modal'+index" style="margin: 10px">
             <Modal with-close-button :title="modal.name" :index="index">
                 <p>
                     {{ modal.description }}
@@ -30,30 +30,6 @@ export default {
         ButtonComponent,
         Modal,
     },
-    computed: {
-        ...mapGetters('modals', [
-            'getModals',
-        ]),
-        modals() {
-            return this.$store.getters['modals/getModals'];
-        }
-    },
-    methods: {
-        ...mapActions('modals', [
-            'addModalAction',
-            'removeModalAction',
-        ]),
-        addModal(modal) {
-            this.addModalAction(modal);
-        },
-        removeModal(i) {
-            this.removeModalAction(i);
-        }
-    },
-    mounted() {
-        this.$root.$on('addModal', (data) => this.addModal(data));
-        this.$root.$on('removeModal', (i) => this.removeModal(i));
-    },
     data() {
         return {
             modalsData: [{
@@ -70,6 +46,27 @@ export default {
                 description: 'Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications. APIs. With a myriad of HTTP utility methods and middleware at your disposal, creating a robust API is quick and easy. Performance. Express provides a thin layer of fundamental web application features, without obscuring Node.js features that you know and love. Frameworks. Many popular frameworks are based on Express.'
             }]
         }
-    }
+    },
+    computed: {
+        ...mapGetters('modals', [
+            'getModals',
+        ]),
+    },
+    mounted() {
+        this.$root.$on('addModal', (data) => this.addModal(data));
+        this.$root.$on('removeModal', (i) => this.removeModal(i));
+    },
+    methods: {
+        ...mapActions('modals', [
+            'addModalAction',
+            'removeModalAction',
+        ]),
+        addModal(modal) {
+            this.addModalAction(modal);
+        },
+        removeModal(i) {
+            this.removeModalAction(i);
+        }
+    },
 }
 </script>
