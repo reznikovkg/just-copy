eeccf31f05ed3794326addcb31b8cc5a44584e7e
Niapoll <Kolya21082001@gmail.com>
2023-03-31 19:24:18 +0000

    Add template project to Vue3

---
+++ vue3/app/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="home">
+        <router-link :to="{ name: 'btn' }">
+            Btn
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeView'
+}
+</script>
---
+++ vue3/components/button/Button.vue
@@ -0,0 +1,33 @@
+<template>
+    <button class="btn" :class="['btn--' + type, 'btn--' + size]" @click="() => actionClick()">
+        <slot>Button</slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        type: {
+            type: String,
+            // default, error, warning, success
+            default: 'default'
+        },
+        size: {
+            type: String,
+            // small, medium, large
+            default: 'medium'
+        }
+
+    },
+    methods: {
+        actionClick() {
+            this.$emit('click')
+        }
+    }
+}
+</script>
+
+<style lang="less">
+@import "styles/styles.less";
+</style>
