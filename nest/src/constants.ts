a940ff1e294699daea26f25ab95a955dbc9d8130
twtzptr <wildhare2020@yandex.ru>
2023-04-02 03:13:21 +0300

    modals server implemented

---
+++ nest/src/constants.ts
@@ -0,0 +1,4 @@
+export const LOWDB = 'lowdb';
+export const DB_FILENAME = 'db.json';
+export const UNEXIST_MODAL_POP_MSG = 'Delete on empty array could be performed';
+export const MODALS_DB_NAME = 'modals';
