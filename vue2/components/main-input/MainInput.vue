b05431a15e403485f5e0f6c9afae484ecd235cc2
Danil Polukazakov <karomoz2@yandex.ru>
2023-04-03 21:26:49 +0300

    First commit

---
+++ vue2/components/main-input/MainInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="val-input-container">
+    <ValueInput :step="0.1" :max="max" :min="min" />
+    Min:<ValueInput :valueInput="min" @create="setMin"></ValueInput>
+    Max:<ValueInput :valueInput="max" @create="setMax"></ValueInput>
+  </div>
+</template>
+
+<script>
+import ValueInput from "../value-input/ValueInput.vue"
+export default {
+  components: {
+    ValueInput,
+  },
+  props: {
+    maxValue: {
+      default: 10,
+      required: false,
+    },
+    minValue: {
+      default: -10,
+      required: false,
+    }
+  },
+  data() {
+    return {
+      max: this.maxValue,
+      min: this.minValue,
+    }
+  },
+  methods: {
+    setMax(max) {
+      this.max = max;
+    },
+    setMin(min) {
+      this.min = min;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./styles/style.scss";
+</style>
