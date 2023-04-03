ab32ffaf497f8f38276d5459e6c56d50915f8185
Рубан Павел <pruban@roox.ru>
2023-04-03 19:40:32 +0300

    Добавил ProgressBar

--- react/react-ts/src/App.tsx
+++ react/react-ts/src/App.tsx
@@ -1,21 +1,22 @@
 import Button from "./components/Button/Button";
 import { notification } from "./components/NotificationManager/NotificationManager";
+import ProgressBar from "./components/ProgressBar/ProgressBar";
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <Button
-          onClick={() =>
-            notification.show({
-              title: "Notification title",
-              content: "Notification body",
-              duration: 5000,
-            })
-          }
-          text={"create"}
-        />
-      </header>
+    <div className="app-container">
+      <Button
+        onClick={() =>
+          notification.show({
+            title: "Notification title",
+            content: "Notification body",
+            duration: 5000,
+          })
+        }
+        text={"create"}
+      />
+
+      <ProgressBar />
     </div>
   );
 }
