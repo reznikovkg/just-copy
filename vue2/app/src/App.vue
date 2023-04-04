efe1a60a97151d6a84ff4178003c7fb580e13b36
mollbil <wollohq@yandex.ru>
2023-04-04 14:09:14 +0300

    first commit

--- vue2/app/src/App.vue
+++ vue2/app/src/App.vue
@@ -1,21 +1,72 @@
 <template>
-  <div id="app">
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-    </nav>
-    <RouterView/>
-  </div>
+  <body>
+    <div id="app">
+      <div class="content">
+        <h3 class="content__title">Лабораторная работа №2</h3>
+        <div class="container">
+          <div class="container__lab-info">
+            <h6 class="info-title">Вариант 8. Аккордеон</h6>
+            <p class="info-author">Борченко Ирина, 61 группа</p>
+          </div>
+          <div class="accordion">
+            <AccorditionComponent 
+            v-for="(accordion__item, i) in accordion"
+            :accordion__item="accordion__item"
+            :index="i" 
+            :key="i"
+            :open="accordion__item.open"
+            @clickHandler="clickHandler"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
 </template>
 
 <script>
+import AccorditionComponent from '../../components/accorditionBorchenko/AccorditionComponent.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AccorditionComponent
+  },
+  data() {
+    return {
+      accordion: [
+        {
+          title: "Мудрость",
+          content: "Sub specie aeternitatis", 
+          open: false
+        },
+        {
+          title: "Вопрос",
+          content: "Когда", 
+          open: false
+        },
+        {
+          title: "11010000 10010110",
+          content: "11010000 10110000 11010000 10110001 11010000 10110000", 
+          open: false
+        }
+      ]
+    }
+  },
+  methods: {
+    clickHandler: function(index) {
+      this.accordion = this.accordion.map((accordion__item,  i) => {
+        if(index === i) {
+          accordion__item.open = !accordion__item.open;
+        }
+        return accordion__item;
+      })
+    }
+  }
 }
 </script>
 
-<style>
-    body {
-        padding: 0;
-        margin: 0;
-    }
+<style lang="less">
+@import "styles/styles.less";
 </style>
+
---
+++ vue2/components/accorditionBorchenko/AccorditionComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="open ? 'accordion__item--open' : 'accordion__item'" @click="$emit('clickHandler', index)">
+    <button class="accordion__item--title">{{ accordion__item.title }}</button>
+    <div class="accordion__item--hidden">
+      <p>{{ accordion__item.content }}</p>
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+    name: 'AccordionItem',
+    props: ['accordion__item', 'index', 'open']
+}
+</script>
+  
+
+<style scoped lang="less">
+@import "styles/styles.less";
+</style>
+  
