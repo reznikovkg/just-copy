f1272271cf56fbc24310248db6ecb55d93afe8c5
BethShel <limishel.e@yandex.ru>
2023-04-05 15:25:15 +0300

    change ProgressShelPage

--- vue2/app/src/views/ProgressShelPage.vue
+++ vue2/app/src/views/ProgressShelPage.vue
@@ -1,101 +1,96 @@
 <template>
   <div class="about">
-    <h1>Progress</h1>
-    <div style="max-width: 500px;">
-    <h2>Progress 1</h2>
-    <ProgressBar
-        v-model="progressOptions1"
-        :showTip="true"
-        ref="ProgressBar1"
-    />
-    <Button @click="() => this.$refs.ProgressBar1.decrease(10)">
-        -10
-    </Button>
-    <Button @click="() => this.$refs.ProgressBar1.increase(10)">
-        +10
-    </Button>
-    <h2>Progress 2</h2>
-    <ProgressBar
-        v-model="progressOptions2"
-        :showTip="false"
-        ref="ProgressBar2"
-    />
-    <Button @click="() => this.$refs.ProgressBar2.decrease(10)">
-        -10
-    </Button>
-    <Button @click="() => this.$refs.ProgressBar2.increase(10)">
-        +10
-    </Button>
-    <h2>Progress 3</h2>
-    <ProgressBar
-        v-model="progressOptions3"
-        :showTip="true"
-        :height="30"
-        :colorFunc=color
-        ref="ProgressBar3"
-    />
-    <Button @click="() => this.$refs.ProgressBar3.decrease(10)">
-        -10
-    </Button>
-    <Button @click="() => this.$refs.ProgressBar3.increase(10)">
-        +10
-    </Button>
-    <h2>Progress 4 (all props)</h2>
-    <ProgressBar
-        v-model="progressOptions4"
-        :showTip="true"
-        :height="30"
-        :colorFunc=color
-        :innerStrokeColor="'black'"
-        :width="300"
-        :changeTime="1"
-        :timingFunction="'cubic-bezier(.29, 1.01, 1, -0.68)'"
-        ref="ProgressBar4"
-    />
-    <Button @click="() => this.$refs.ProgressBar4.decrease(20)">
-        -20
-    </Button>
-    <Button @click="() => this.$refs.ProgressBar4.increase(20)">
-        +20
-    </Button>
-    <h2>Progress 5 (all props)</h2>
-    <ProgressBar
-        v-model="progressOptions5"
-        :showTip="true"
-        :height="30"
-        :colorFunc=color
-        :innerStrokeColor="'black'"
-        :width="300"
-        :changeTime="1"
-        :timingFunction="'steps(6, end)'"
-        ref="ProgressBar5"
-    />
-    <Button @click="() => this.$refs.ProgressBar5.decrease(20)">
-        -20
-    </Button>
-    <Button @click="() => this.$refs.ProgressBar5.increase(20)">
-        +20
-    </Button>
+    <h1>Прогресс линии</h1>
+      <div style="max-width: 600px;">
+      <h2>Первая прогресс линия</h2>
+      <h3>Отсутствуют параметры, но использовано хранилище</h3> 
+      <ProgressShl v-model="firstProgress" ref="ProgressFirst" />
+      <ButtonShl @click="ChangeFirstProgress(-10)">
+          -10
+      </ButtonShl>
+      <ButtonShl @click="ChangeFirstProgress(10)">
+          +10
+      </ButtonShl>
+      <h2>Вторая прогресс линия</h2>
+      <h3>Отсутствует текст процентов</h3>
+      <ProgressShl v-model="secondProgress" :showPercentage="false" ref="ProgressSecond" />
+      <ButtonShl @click="ChangeSecondProgress(-15)">
+          -15
+      </ButtonShl>
+      <ButtonShl @click="ChangeSecondProgress(15)">
+          +15
+      </ButtonShl>
+      <h2>Третья прогресс линия</h2>
+      <h3>Изменена высота, ширина и с функцией анимации: cubic-bezier(.29, 1.01, 1, -0.68)</h3>
+      <ProgressShl
+          v-model="thirdProgress"
+          :progressHeight="30"
+          :progressWidth="500"
+          :timingFunction="'cubic-bezier(.29, 1.01, 1, -0.68)'"
+          ref="ProgressThird"
+      />
+      <ButtonShl @click="ChangeThirdProgress(-20)">
+          -20
+      </ButtonShl>
+      <ButtonShl @click="ChangeThirdProgress(20)">
+          +20
+      </ButtonShl>
+      <h2>Четвертая прогресс линия</h2>
+      <h3>Изменена скорость анимации, с функцией анимации: steps(6, end), изменен цвет прогресс линии</h3>
+      <ProgressShl
+          v-model="fourthProgress"
+          :changeTime="1"
+          :timingFunction="'steps(6, end)'"
+          :colorFunc=CustomColorMethod
+          ref="ProgressFourth"
+      />
+      <ButtonShl @click="ChangeFourthProgress(-25)">
+          -25
+      </ButtonShl>
+      <ButtonShl @click="ChangeFourthProgress(25)">
+          +25
+      </ButtonShl>
+      <h2>Пятая прогресс линия</h2>
+      <h3>Изменен цвет подложки</h3>
+      <ProgressShl
+          v-model="fifthProgress"
+          :innerStrokeColor="'#ffa500'"
+          ref="ProgressFifth"
+      />
+      <ButtonShl @click="ChangeFifthProgress(-30)">
+          -30
+      </ButtonShl>
+      <ButtonShl @click="ChangeFifthProgress(30)">
+          +30
+      </ButtonShl>
     </div>
   </div>
 </template>
   
   <script>
-  import ProgressBar from "../../../components/progressShel/ProgressShel.vue";
-  import Button from "../../../components/button/Button.vue";
+  import ButtonShl from "../../../components/button/Button.vue";
+  import ProgressShl from "../../../components/progressShel/ProgressShel.vue";
   import { mapGetters, mapActions } from "vuex";
   
   export default {
     name: "SelectPage",
     components: {
-      ProgressBar,
-      Button
+      ProgressShl,
+      ButtonShl
+    },
+    data() {
+      return {
+        secondProgress: 44,
+        thirdProgress: 78,
+        fourthProgress: 19,
+        fifthProgress: 56
+      }
     },
     computed: {
       ...mapGetters('progress', [
         'getProgressValue'
       ]),
-      progressOptions1: {
+      firstProgress: {
         get() {
           return this.getProgressValue;
         },
@@ -104,21 +99,34 @@
         }
       }
     },
-    data() {
-      return {
-        progressOptions2: 80,
-        progressOptions3: 0,
-        progressOptions4: 12,
-        progressOptions5: 25
-      }
-    },
     methods: {
-      color(percentage) {
-        return `hsl(${percentage * 1.2} 50% 50%)`;
-      },
       ...mapActions('progress', [
         'setProgressValue'
       ]),
+      ChangeFirstProgress(changeValue) {
+        this.$refs.ProgressFirst.changePercentage(changeValue);
+      },
+      ChangeSecondProgress(changeValue) {
+        this.$refs.ProgressSecond.changePercentage(changeValue);
+      },
+      ChangeThirdProgress(changeValue) {
+        this.$refs.ProgressThird.changePercentage(changeValue);
+      },
+      ChangeFourthProgress(changeValue) {
+        this.$refs.ProgressFourth.changePercentage(changeValue);
+      },
+      ChangeFifthProgress(changeValue) {
+        this.$refs.ProgressFifth.changePercentage(changeValue);
+      },
+      CustomColorMethod(percentage) {
+        if (percentage < 30) {
+          return '#909399'
+        }
+        if (percentage < 70) {
+          return '#e6a23c'
+        }
+        return '#67c23a'
+      }
     }
   }
   </script>
