0b6607ffa339859a0ec5988d921a7ee8942600ec
Roman Shimkin <roma.shimkin@yandex.ru>
2023-04-05 19:41:27 +0300

    added logic to store and component, added styles

--- vue2/app/src/store/notificationShimkin.js
+++ vue2/app/src/store/notificationShimkin.js
@@ -7,5 +7,45 @@ export default {
   namespaced: true,
   state: {
     notifications: [],
+    idGenerator: 0
+  },
+  mutations: {
+    addNotification: function (state, payload) {
+      state.notifications.push({
+        id: ++state.idGenerator,
+        title: payload.title,
+        content: payload.content,
+        type: payload.type,
+        size: payload.size ?? "normal",
+      });
+      console.log(this)
+      const idToDelete = state.idGenerator;
+      if (payload.delay) {
+        setTimeout(() => {
+          state.notifications = state.notifications.filter(notification => notification.id!== idToDelete);
+        }, payload.delay);
+      }
+    },
+    removeNotification: (state, payload) => {
+      state.notifications = state.notifications.filter(notification => notification.id!== payload);
+    },
+    clearNotifications: (state) => {
+      state.notifications = [];
+    }
+  },
+  getters: {
+    getNotifications: (state) => state.notifications,
+    getId: (state) => state.idGenerator,
+  },
+  actions: {
+    addNotification: ({ commit }, payload) => {
+      commit("addNotification", payload);
+    },
+    removeNotification: ({ commit }, payload) => {
+      commit("removeNotification", payload);
+    },
+    clearNotifications: ({ commit }) => {
+      commit("clearNotifications");
+    }
   }
 };
--- vue2/components/notificationShimkin/NotificationShimkin.vue
+++ vue2/components/notificationShimkin/NotificationShimkin.vue
@@ -1,9 +1,42 @@
 <template>
-    <div>Notification component by Shimkin Roman 62</div>
+	<div :class="['notif__container']">
+		<div class="notif__group">
+			<TransitionGroup name="animation" tag="div" class="notif__group">
+				<div 
+				:class="[`notif__item`, `notif__item--${notif.type}`, `notif__item--${notif.size}`]"
+				v-for="notif in getNotifications" 
+				:key="notif.id">
+					<h2 class="notif__title">{{ notif.title }}</h2>
+					<div class="notif__content">
+						<p>{{ notif.content }}</p>
+					</div>
+					<div class="notif__close"
+					@click="() => removeNotification(notif.id)">
+					✖</div>
+				</div>
+			</TransitionGroup>
+		</div>
+	</div>
 </template>
 
 <script>
-    export default {
-        name: "notificationShimkin"
-    }
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+	name: "notificationShimkin",
+	computed: {
+		...mapGetters("notificationShimkin", [
+			"getNotifications",
+		]),
+	},
+	methods: {
+    ...mapActions("notificationShimkin",[
+      "removeNotification"
+    ]),
+  },
+};
 </script>
+
+<style scoped lang="less">
+@import "styles/styles.less";
+</style>
