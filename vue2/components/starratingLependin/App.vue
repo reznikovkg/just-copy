c0e94caf979d2e5aa83b159700427fa0b6b298d4
Lependin Alexandr <117677105+LegitBen@users.noreply.github.com>
2023-11-29 22:06:49 +0300

    Add files via upload

---
+++ vue2/components/starratingLependin/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+    <div>
+      <star-rating :ratingProps="2.5" :starLimitProps="10" />
+      <star-rating
+        :ratingProps="6.5"
+        :starLimitProps="8"
+        :isCountProps="false"
+      />
+      <star-rating :ratingProps="3" :starLimitProps="5" />
+    </div>
+  </div>
+</template>
+
+<script>
+import StarRating from "./components/StarRating.vue";
+export default {
+  name: "App",
+  components: {
+    StarRating,
+  },
+};
+</script>
+
+<style>
+.wrapper {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
