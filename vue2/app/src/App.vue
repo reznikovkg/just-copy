f0640a3c42706f11e22af9568ed1dff32024b4d2
Stanislav Muntyanu <stasean21@yandex.com>
2023-12-18 02:31:37 +0300

    Мунтяну Станислав, Лабораторная работа №1, Вариант 7 "Модальное окно" Vue

--- vue2/app/src/App.vue
+++ vue2/app/src/App.vue
@@ -1,21 +1,53 @@
 <template>
-  <div id="app">
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-    </nav>
-    <RouterView/>
+  <div class="page">
+    <button class="show-modal-button" @click="showModal">Показать модальное окно</button>
+    <ModalWindow ref="modal"></ModalWindow>
   </div>
 </template>
 
 <script>
+// Используйте относительный путь с префиксом '@'
+import ModalWindow from './components/Modal-window.vue';
+
 export default {
-  name: 'App'
-}
+  name: 'App',
+  components: {
+    ModalWindow,
+  },
+  methods: {
+    showModal() {
+      this.$refs.modal.show = true;
+    },
+  },
+};
 </script>
 
-<style>
-    body {
-        padding: 0;
-        margin: 0;
-    }
+<style lang="scss">
+ body {
+font-family: 'Source Sans Pro', sans-serif;
+margin: 0;
+padding: 0;
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+}
+.page {
+position: relative;
+width: 100%;
+min-height: 100%;
+}
+/* Стили для кнопки */
+.show-modal-button {
+background-color: #0971c7;
+color: #fff;
+border: none;
+padding: 10px 15px;
+font-size: 16px;
+border-radius: 8px;
+cursor: pointer;
+}
+.show-modal-button:hover {
+background-color: #2a4cc7;
+}
 </style>
