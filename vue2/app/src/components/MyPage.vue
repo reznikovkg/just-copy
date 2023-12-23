aef6d38e17f7dfebc91b50fa007562ad7e5d7951
Stanislav Muntyanu <95855695+stasean21@users.noreply.github.com>
2023-12-23 18:27:44 +0300

    Add files via upload

---
+++ vue2/app/src/components/MyPage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+      <ModalWindow :show="isModalVisible" :title="modalTitle" :content="modalContent" @close="closeModal">
+        <!-- Передача данных через слоты -->
+        <template v-slot:title>
+          <h3 class="custom-title">{{ modalTitle }}</h3>
+        </template>
+        <template v-slot:body>
+          <div class="custom-content">{{ modalContent }}</div>
+        </template>
+        <!-- Футер может оставаться без изменений или изменяться по необходимости -->
+      </ModalWindow>
+      <button class="show-modal-button" @click="openModal">Открыть модальное окно</button>
+    </div>
+  </template>
+  
+  <script>
+  import ModalWindow from "@/components/Modal-window.vue";
+  
+  export default {
+    components: {
+      ModalWindow,
+    },
+    data() {
+      return {
+        isModalVisible: false,
+        modalTitle: "Точно модальное окно?", // Данные для заголовка
+        modalContent: "Новое содержание", // Данные для содержания
+      };
+    },
+    methods: {
+      openModal() {
+        this.isModalVisible = true;
+      },
+      closeModal() {
+        this.isModalVisible = false;
+      },
+    },
+  };
+  </script>
+  
+
+
+
+<style scoped>
+/* Импорт файла со стилями */
+@import "@/styles.css";
+</style>
