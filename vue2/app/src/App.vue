304220b4a3406716e93c7943226a05eec588b3c9
Stanislav Muntyanu <95855695+stasean21@users.noreply.github.com>
2023-12-23 18:28:29 +0300

    Add files via upload

--- vue2/app/src/App.vue
+++ vue2/app/src/App.vue
@@ -1,53 +1,22 @@
 <template>
-  <div class="page">
-    <button class="show-modal-button" @click="showModal">Показать модальное окно</button>
-    <ModalWindow ref="modal"></ModalWindow>
+  <div id="app">
+    <MyPage />
   </div>
 </template>
 
+  
 <script>
-// Используйте относительный путь с префиксом '@'
-import ModalWindow from './components/Modal-window.vue';
+import MyPage from "@/components/MyPage.vue"; // Обновите путь к компоненту
 
 export default {
-  name: 'App',
   components: {
-    ModalWindow,
-  },
-  methods: {
-    showModal() {
-      this.$refs.modal.show = true;
-    },
+    MyPage,
   },
 };
 </script>
 
-<style lang="scss">
- body {
-font-family: 'Source Sans Pro', sans-serif;
-margin: 0;
-padding: 0;
-height: 100vh;
-display: flex;
-justify-content: center;
-align-items: center;
-}
-.page {
-position: relative;
-width: 100%;
-min-height: 100%;
-}
-/* Стили для кнопки */
-.show-modal-button {
-background-color: #0971c7;
-color: #fff;
-border: none;
-padding: 10px 15px;
-font-size: 16px;
-border-radius: 8px;
-cursor: pointer;
-}
-.show-modal-button:hover {
-background-color: #2a4cc7;
-}
+ 
+<style scoped>
+/* Импорт файла со стилями */
+@import "@/styles.css";
 </style>
