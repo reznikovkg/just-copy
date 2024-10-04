b67769946a2a0dced0f050c0028dcdc19a520a35
Lilya01 <Lilya_Kireeva01@mail.ru>
2024-10-04 17:41:36 +0300

    добавлено круговое меню навигации

--- vue2/app/src/router/index.js
+++ vue2/app/src/router/index.js
@@ -1,106 +1,131 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/btn',
-    name: 'btn',
-    component: () => import(/* webpackChunkName: "btn" */ '../views/BtnPage.vue')
-  },
-  {
-    path: '/exp',
-    name: 'exp',
-    component: () => import(/* webpackChunkName: "btn" */ '../views/ExpressPage.vue')
-  },
-  {
-    path: '/checkbox',
-    name: 'checkbox',
-    component: () => import(/* webpackChunkName: "checkbox" */ '../views/CheckBoxPage.vue')
-  },
-  {
-    path: '/select',
-    name: 'select',
-    component: () => import(/* webpackChunkName: "select" */ '../views/SelectPage.vue')
-  },
-  {
-    path: '/notifications',
-    name: 'notifications',
-    component: () => import(/* webpackChunkName: "select" */ '../views/NotificationPage.vue')
-  },
-  {
-    path: '/modal',
-    name: 'modal',
-    component: () => import(/* webpackChunkName: "select" */ '../views/ModalPage/ModalPage.vue')
-  },
-  {
-    path: '/progress',
-    name: 'progress',
-    component: () => import(/* webpackChunkName: "progress" */ '../views/ProgressPage.vue')
-  },
-  {
-    path: '/Tabs',
-    name: 'tabs',
-    component: () => import(/* webpackChunkName: "select" */ '../views/TabsPage.vue')
-  },
-  {
-    path: '/modal2',
-    name: 'modal2',
-    component: () => import(/* webpackChunkName: "modal2" */ '../views/ModalPage.vue')
-  },
-  {
-    path: '/loader',
-    name: 'loader',
-    component: () => import(/* webpackChunkName: "loader" */ '../views/LoaderPage.vue')
-  },
-  {
-    path: '/loaderSemenov',
-    name: 'loaderSemenov',
-    component: () => import(/* webpackChunkName: "loader" */ '../views/LoaderPageSemenov.vue')
-  },
-  {
-    path: '/progress-an',
-    name: 'progressAn',
-    component: () => import(/* webpackChunkName: "loader" */ '../views/ProgressAnPage.vue')
-  },
-  {
-    path: '/accordion',
-    name: 'accordion',
-    component: () => import(/* webpackChunkName: "accordion" */ '../views/AccordionPage.vue')
-  },
-  {
-    path: '/tabsKiseleva',
-    name: 'tabsKiseleva',
-    component: () => import('../views/TabsKiseleva.vue')
-  },
-  {
-    path: '/tabsSviridov',
-    name: 'tabsSviridov',
-    component: () => import(/* webpackChunkName: "modal2" */ '../views/TabsSviridov.vue')
-  },
-  {
-    path: '/mainInput',
-    name: 'mainInput',
-    component: () => import(/* webpackChunkName: "mainInput" */ '../views/MainInput.vue')
- },
- {
-    path: '/accordionStaysPage',
-    name: 'accordionStaysPage',
-    component: () => import(/* webpackChunkName: "modal2" */ '../views/AccordionStaysPage.vue')
-  },
-]
+	{
+		path: "/",
+		name: "home",
+		component: HomeView,
+	},
+	{
+		path: "/btn",
+		name: "btn",
+		component: () =>
+			import(/* webpackChunkName: "btn" */ "../views/BtnPage.vue"),
+	},
+	{
+		path: "/exp",
+		name: "exp",
+		component: () =>
+			import(/* webpackChunkName: "btn" */ "../views/ExpressPage.vue"),
+	},
+	{
+		path: "/checkbox",
+		name: "checkbox",
+		component: () =>
+			import(/* webpackChunkName: "checkbox" */ "../views/CheckBoxPage.vue"),
+	},
+	{
+		path: "/select",
+		name: "select",
+		component: () =>
+			import(/* webpackChunkName: "select" */ "../views/SelectPage.vue"),
+	},
+	{
+		path: "/notifications",
+		name: "notifications",
+		component: () =>
+			import(/* webpackChunkName: "select" */ "../views/NotificationPage.vue"),
+	},
+	{
+		path: "/modal",
+		name: "modal",
+		component: () =>
+			import(
+				/* webpackChunkName: "select" */ "../views/ModalPage/ModalPage.vue"
+			),
+	},
+	{
+		path: "/progress",
+		name: "progress",
+		component: () =>
+			import(/* webpackChunkName: "progress" */ "../views/ProgressPage.vue"),
+	},
+	{
+		path: "/Tabs",
+		name: "tabs",
+		component: () =>
+			import(/* webpackChunkName: "select" */ "../views/TabsPage.vue"),
+	},
+	{
+		path: "/modal2",
+		name: "modal2",
+		component: () =>
+			import(/* webpackChunkName: "modal2" */ "../views/ModalPage.vue"),
+	},
+	{
+		path: "/loader",
+		name: "loader",
+		component: () =>
+			import(/* webpackChunkName: "loader" */ "../views/LoaderPage.vue"),
+	},
+	{
+		path: "/loaderSemenov",
+		name: "loaderSemenov",
+		component: () =>
+			import(/* webpackChunkName: "loader" */ "../views/LoaderPageSemenov.vue"),
+	},
+	{
+		path: "/progress-an",
+		name: "progressAn",
+		component: () =>
+			import(/* webpackChunkName: "loader" */ "../views/ProgressAnPage.vue"),
+	},
+	{
+		path: "/accordion",
+		name: "accordion",
+		component: () =>
+			import(/* webpackChunkName: "accordion" */ "../views/AccordionPage.vue"),
+	},
+	{
+		path: "/tabsKiseleva",
+		name: "tabsKiseleva",
+		component: () => import("../views/TabsKiseleva.vue"),
+	},
+	{
+		path: "/tabsSviridov",
+		name: "tabsSviridov",
+		component: () =>
+			import(/* webpackChunkName: "modal2" */ "../views/TabsSviridov.vue"),
+	},
+	{
+		path: "/mainInput",
+		name: "mainInput",
+		component: () =>
+			import(/* webpackChunkName: "mainInput" */ "../views/MainInput.vue"),
+	},
+	{
+		path: "/accordionStaysPage",
+		name: "accordionStaysPage",
+		component: () =>
+			import(
+				/* webpackChunkName: "modal2" */ "../views/AccordionStaysPage.vue"
+			),
+	},
+	{
+		path: "/radialMenu",
+		name: "radialMenu",
+		component: () => import("../views/RadialMenu.vue"),
+	},
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+	mode: "history",
+	base: process.env.BASE_URL,
+	routes,
+});
 
-export default router
+export default router;
---
+++ vue2/app/src/views/RadialMenu.vue
@@ -0,0 +1,78 @@
+html
+<template>
+	<div>
+		<RadialMenu :items="menuItems" />
+	</div>
+</template>
+
+<script>
+import RadialMenu from "../../../components/radialMenu/RadialMenu.vue";
+
+export default {
+	components: {
+		RadialMenu,
+	},
+	data() {
+		return {
+			menuItems: [
+				{
+					icon: "https://cdn.icon-icons.com/icons2/3824/PNG/512/statistics_growth_business_analytics_diagram_icon_233861.png",
+					color: "rgb(255, 232, 165)",
+					name: "Item 1",
+					callback: () => {
+						alert("Item 1 clicked!");
+					},
+				},
+				{
+					icon: "https://cdn.icon-icons.com/icons2/3824/PNG/512/fox_web_fire_website_webpage_browser_icon_233852.png",
+					color: "#ff7741",
+					name: "Item 2",
+					callback: () => {
+						alert("Item 2 clicked!");
+					},
+				},
+				{
+					icon: "https://cdn.icon-icons.com/icons2/3824/PNG/512/play_market_shopping_player_store_icon_233841.png",
+					color: "#7f74ff",
+					name: "Item 3",
+					callback: () => {
+						alert("Item 3 clicked!");
+					},
+				},
+				{
+					icon: "https://cdn.icon-icons.com/icons2/3824/PNG/512/seo_app_browser_allo_internet_web_icon_233847.png",
+					color: "#4fe6ff",
+					name: "Item 4",
+					callback: () => {
+						alert("Item 4 clicked!");
+					},
+				},
+				{
+					icon: "https://cdn.icon-icons.com/icons2/3824/PNG/512/team_users_communication_person_group_on_icon_233863.png",
+					color: "#59ff59",
+					name: "Item 5",
+					callback: () => {
+						alert("Item 5 clicked!");
+					},
+				},
+				{
+					icon: "https://cdn.icon-icons.com/icons2/3824/PNG/512/apps_application_app_smartphone_drupal_icon_233840.png",
+					color: "#849eec",
+					name: "Item 6",
+					callback: () => {
+						alert("Item 6 clicked!");
+					},
+				},
+				{
+					icon: "https://cdn.icon-icons.com/icons2/3824/PNG/512/brand_yelp_internet_network_icon_233865.png",
+					color: "#ff9f9f",
+					name: "Item 7",
+					callback: () => {
+						alert("Item 7 clicked!");
+					},
+				},
+			],
+		};
+	},
+};
+</script>
